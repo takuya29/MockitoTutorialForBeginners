50869f2d152c89e98cb5d0cde74bc83fb8c53cdd
Takuya Shimada <gct3860@gmail.com>
2022-12-03 15:42:57 +0900

    Step8: assertArrayEquals

---
+++ src/test/java/com/in28minutes/junit/helper/ArraysCompareTest.java
@@ -0,0 +1,18 @@
+package com.in28minutes.junit.helper;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class ArraysCompareTest {
+
+    @Test
+    public void testArraySort_RandomArray() {
+        int[] numbers = {1, 2, 12, 4};
+        int[] expected = {1, 2, 4, 12};
+        Arrays.sort(numbers);
+        assertArrayEquals(expected, numbers);
+    }
+}
