05c3c778b8efc469dc3d751027be0a2ca091e646
Takuya Shimada <gct3860@gmail.com>
2022-12-03 15:49:06 +0900

    Step10: set timeout

--- src/test/java/com/in28minutes/junit/helper/ArraysCompareTest.java
+++ src/test/java/com/in28minutes/junit/helper/ArraysCompareTest.java
@@ -21,4 +21,13 @@ public class ArraysCompareTest {
         int[] numbers = null;
         Arrays.sort(numbers);
     }
+
+    @Test(timeout = 100)
+    public void testSort_Performance() {
+        int[] array = {12, 23, 4};
+        for (int i = 0; i < 1000000; i++) {
+            array[0] = i;
+            Arrays.sort(array);
+        }
+    }
 }
